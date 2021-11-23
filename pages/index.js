d064a0eb9ae84424ba75ce30fca22be57b5ba140
RockBacon9922 <58570245+RockBacon9922@users.noreply.github.com>
2021-11-23 19:00:27 +0000

    Footer is now displayed on pages TODO a menu bar!!

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,8 @@
-import { Router } from "next/dist/client/router";
 import Head from "next/head";
-import { useRouter } from "next/dist/client/router";
 import Image from "next/image";
 
 export default function Home() {
-  const router = useRouter()
-  function togithub() {
-    router.push("https://github.com/rockbacon9922")
-  }
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
@@ -16,23 +11,12 @@ export default function Home() {
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <img src="/Logo.png" alt="Bastion logo" layout="fixed"></img>
+        <Image layout="intrinsic" width={3487} height={2621} src="/Logo.png" alt="Bastion logo"/>
         <h1 className="text-6xl md:text-9xl lg:text-{900px}">BASTION</h1>
         <br/>
         <br/>
 
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <div
-          className="flex flex-col items-center justify-center text-center"
-        >
-          <p onClick={togithub} onKeyPress={togithub}className="hover:text-gray-500">A RockBacon Website</p>
-        <p>
-        A Young Enterprise Company
-        </p>
-        </div>
-      </footer>
     </div>
   )
 }
