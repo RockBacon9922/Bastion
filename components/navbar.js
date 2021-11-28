0c060b938eb36f2e5985fc15aebc9d0915ba7b02
RockBacon9922 <58570245+RockBacon9922@users.noreply.github.com>
2021-11-28 00:27:39 +0000

    Added a prefetch feature to make website quicker

--- components/navbar.js
+++ components/navbar.js
@@ -2,20 +2,22 @@ import { useRouter } from "next/dist/client/router"
 
 export default () => {
     const routeTo = useRouter().push
+    const router = useRouter()
 
     return <navbar className="h-14 flex gap-7 shadow-md fixed w-full z-50 bg-white">
-        <img quality={5} layout="responsive" src="/Logo.svg" alt="Bastion logo" onClick={() => routeTo("/")} onKeyPress={() => routeTo("/")}/>
+        <img quality={5} layout="responsive" src="/Logo.svg" alt="Bastion logo" className="hover:bg-gray-100" onClick={() => routeTo("/")} onKeyPress={() => routeTo("/")}/>
         <ROCK txt="Coasters" route="/Coasters"/>
         <ROCK txt="Tea Lights" route="/TeaLights"/>
     </navbar>
 }
 
 const ROCK = function(props){
-    function toRoute() {
+    function routeTo() {
         router.push(props.route)
         }
     const router = useRouter()
-    return <div className="flex p-4">
-        <div role="button" onClick={toRoute} onKeyPress={toRoute} className="text-center self-center" alt={props.txt}>{props.txt}</div>
+    router.prefetch(props.route)
+    return <div className="flex p-4 hover:bg-gray-100">
+        <div role="button" onClick={routeTo} onKeyPress={routeTo} className="text-center self-center text-xl" alt={props.txt}>{props.txt}</div>
     </div>
 }
