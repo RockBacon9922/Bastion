e54bab86ff9d623a3503ef4b294fcb5b5fc13be7
RockBacon9922 <58570245+RockBacon9922@users.noreply.github.com>
2021-11-28 00:26:41 +0000

    Updated logo.ico

--- components/head.js
+++ components/head.js
@@ -3,7 +3,7 @@ import Head from "next/dist/shared/lib/head";
 export default () => {
     return <Head>
     <title>Bastion</title>
-    <link rel="icon" href="/Logo.ico?v=3" />
+    <link rel="icon" href="/Logo.ico?v=4" />
     <meta name="description" content="Bastion A Young Enterprise Company"/>
     <meta name="keywords" content="Bastion, Young, Enterprise, Company, Bethany, School"/>
     </Head>}
