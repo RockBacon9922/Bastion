50a26b3af932e4147c4bf0a421da434dc2a3ad50
RockBacon9922 <58570245+RockBacon9922@users.noreply.github.com>
2021-11-28 00:28:27 +0000

    Added new product pages and subsaquent assets

---
+++ pages/Coasters.js
@@ -0,0 +1,10 @@
+export default () => {
+
+    return <main>
+
+        <article>
+
+
+        </article>
+    </main>
+}
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default () => {
 
   return (
     <main className="flex flex-col">
-        <Image quality={5} layout="intrinsic" width={3487} height={2621} src="/Logo.svg" alt="Bastion logo" className="slide-in-and-rotate" />
+        <Image quality={5} loading="eager" layout="intrinsic" width={3487} height={2621} src="/Logo.svg" alt="Bastion logo" className="slide-in-and-rotate" />
     </main>
   )
 }
