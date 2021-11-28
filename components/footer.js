1543a244b156787483180bd8a5d2820a7daf9bc9
RockBacon9922 <58570245+RockBacon9922@users.noreply.github.com>
2021-11-28 00:26:10 +0000

    Removed RockBacon and added a mailto

--- components/footer.js
+++ components/footer.js
@@ -6,13 +6,16 @@ export default () => {
     function togithub() {
     router.push("https://github.com/rockbacon9922")
     }
+    function mailToUs() {
+      window.open("mailto:Bastion@bethanyschool.org.uk")
+    }
 
     return   <footer className="flex items-center justify-center w-full h-24 border-t flex-col text-center">
-    <p onClick={togithub} onKeyPress={togithub} className="hover:text-gray-500">
-      A RockBacon Website
-    </p>
     <p>
     A Young Enterprise Company
+    </p>
+    <p onClick={mailToUs} onKeyPress={mailToUs}>
+      Bastion@bethanyschool.org.uk
     </p>
   </footer>
 }
