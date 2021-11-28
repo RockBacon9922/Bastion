0386eb114b8a8455863a0cf3796ca48048c30d45
RockBacon9922 <58570245+RockBacon9922@users.noreply.github.com>
2021-11-28 10:48:41 +0000

    Stop index from relying on next/image
    next/image can't be used in static exports. So has to be removed

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
-import Image from "next/image";
-
 export default () => {
 
   return (
     <main className="flex flex-col">
-        <Image quality={5} loading="eager" layout="intrinsic" width={3487} height={2621} src="/Logo.svg" alt="Bastion logo" className="slide-in-and-rotate" />
+        <img loading="eager" src="/Logo.svg" alt="Bastion logo" className="slide-in-and-rotate" />
     </main>
   )
 }
