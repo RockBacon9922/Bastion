639d7fc59233e0b311632d871148fefc007c8ec2
RockBacon9922 <58570245+RockBacon9922@users.noreply.github.com>
2021-12-09 21:22:22 +0000

    finished

--- pages/TeaLights.js
+++ pages/TeaLights.js
@@ -1,5 +1,9 @@
 export default () => {
 
+    function ToProduct() {
+        window.open("https://www.ye-tradingstation.org.uk/product/standard-tea-light")
+      }
+
     return <main>
         <link rel="preload" as="image" href="/teaLights.png"/>
         <div className="flex flex-col m-4">
@@ -9,12 +13,13 @@ export default () => {
                     <h1 className="text-3xl">Tea Lights</h1>
                     <br/>
                     <h2>
-                    Made out of Ash or Birch.
-                    Come in singles or Deluxe edition which comprises of 2 tea lights with scented candles.
-                    Finished with tung which is obtained by pressing the seed from the nut of the tung tree.
-                    Natural so is not flammable.
-                    Water resistant. Hand made. High quality.
+                    Hand made out of high quality Ash or Birch.
+                    Beautifully finished with tung oil (sometimes known as china wood)
+                    which is obtained by pressing the seed from the nut of the tung tree.
+                    The natural finish gives it inflammable properties and water resistant properties.
                     </h2>
+                    <br />
+                    <button className="border-2 border-black pl-2 pr-2 p-2" onClick={ToProduct}>Purchase Here!</button>
                 </article>
             </div>
         </div>
