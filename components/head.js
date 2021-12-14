9af10658782160f5ba9e7ccc83f72c8e9ee204ca
RockBacon9922 <58570245+RockBacon9922@users.noreply.github.com>
2021-12-14 18:47:39 +0000

    fixed issue

--- components/head.js
+++ components/head.js
@@ -6,13 +6,11 @@ export default () => {
     <link rel="icon" href="/Logo.ico?v=4" />
     <meta name="description" content="Bastion A Young Enterprise Company"/>
     <meta name="keywords" content="Bastion, Young, Enterprise, Company, Bethany, School"/>
-    <script>
     <script type="text/javascript">
     (function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
     })(window, document, "clarity", "script", "9qqnxy8u9e");
-    </script>
     </script>
     </Head>}
