b341f890418ac0fc19e98bda35fab89fc23d14ef
RockBacon9922 <58570245+RockBacon9922@users.noreply.github.com>
2021-12-14 14:39:20 +0000

    Changed due to account issues

--- components/head.js
+++ components/head.js
@@ -7,10 +7,12 @@ export default () => {
     <meta name="description" content="Bastion A Young Enterprise Company"/>
     <meta name="keywords" content="Bastion, Young, Enterprise, Company, Bethany, School"/>
     <script>
-        (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "9ch1ll7eon");
+    <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "9qqnxy8u9e");
+    </script>
     </script>
     </Head>}
