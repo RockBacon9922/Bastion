0bc46236e5fa8f47c0b178ca412389e1160e9eb0
RockBacon9922 <58570245+RockBacon9922@users.noreply.github.com>
2021-12-14 14:37:07 +0000

    added telementary from clarity. Microsoft

--- components/head.js
+++ components/head.js
@@ -6,4 +6,11 @@ export default () => {
     <link rel="icon" href="/Logo.ico?v=4" />
     <meta name="description" content="Bastion A Young Enterprise Company"/>
     <meta name="keywords" content="Bastion, Young, Enterprise, Company, Bethany, School"/>
+    <script>
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "9ch1ll7eon");
+    </script>
     </Head>}
