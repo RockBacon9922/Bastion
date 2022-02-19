4c3718077ba65c4388762bd7ab5328348469396e
RockBacon9922 <58570245+RockBacon9922@users.noreply.github.com>
2022-02-19 21:11:16 +0000

    added color theme

--- components/head.js
+++ components/head.js
@@ -6,4 +6,5 @@ export default () => {
     <link rel="icon" href="/Logo.ico?v=4" />
     <meta name="description" content="Bastion A Young Enterprise Company"/>
     <meta name="keywords" content="Bastion, Young, Enterprise, Company, Bethany, School"/>
+    <meta name="theme-color" content="#FFFFFF"/>
     </Head>}
